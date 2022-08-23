77cb70a822a26069734594af60b1b2a64bd62771
JRC91 <jimcarmody91@gmail.com>
2022-08-22 20:44:21 -0400

    fixed login

--- client/index.js
+++ client/index.js
@@ -4,11 +4,11 @@ import {Provider} from 'react-redux'
 import store from '../store/'
 import Routes from './Routes'
 import {BrowserRouter as Router} from 'react-router-dom'
-
+import history from './history'
 
 render(
   <Provider store={store} >
-  <Router>
+  <Router history={history}>
   <Routes />
   </Router>
   </Provider>,
--- store/auth.js
+++ store/auth.js
@@ -1,59 +1,66 @@
-import axios from 'axios'
+import axios from "axios";
 
-const initialState = {}
+const initialState = {};
 
-const TOKEN ='token'
+const TOKEN = "token";
 
-const SET_AUTH = 'SET_AUTH'
+const SET_AUTH = "SET_AUTH";
 
-const setAuth = auth =>({
+const SetAuth = (auth) => ({
   type: SET_AUTH,
-  auth
-})
+  auth,
+});
 
-export function TokenThunk () {
-  async (dispatch) =>{
-    try{
-      console.log('tokenThunk')
-    const token = window.localStorage.getItem(TOKEN)
-    if (token) {
-      const response = await axios.get('/auth/me', {
-        headers: {
-          authorization: token
-        }
-      })
-      return dispatch(setAuth(response.data))
+export function TokenThunk() {
+  return async (dispatch) => {
+    try {
+      console.log("tokenThunk");
+      const token = window.localStorage.getItem(TOKEN);
+      if (token) {
+        const response = await axios.get("/auth/me", {
+          headers: {
+            authorization: token,
+          },
+        });
+        return dispatch(SetAuth(response.data));
+      }
+    } catch (err) {
+      console.log(err);
     }
-  }
-  catch(err){console.log(err)}
-}}
-
-export const authenticate = (username, password, method) => async dispatch => {
-  try {
-    const res = await axios.post(`/auth/${method}`, {username, password})
-    window.localStorage.setItem(TOKEN, res.data.token)
-    dispatch(TokenThunk())
-  } catch (authError) {
-    return dispatch(setAuth({error: authError}))
-  }
+  };
 }
+
+export const authenticate =
+  (username, password, method) => async (dispatch) => {
+    try {
+      const res = await axios.post(`/auth/${method}`, {
+        username,
+        password,
+      });
+      window.localStorage.setItem(TOKEN, res.data.token);
+      dispatch(TokenThunk());
+    } catch (authError) {
+      return dispatch(SetAuth({ error: authError }));
+    }
+  };
 
 export const logout = () => {
-  window.localStorage.removeItem(TOKEN)
-  history.push('/login')
-  return {
-    type: SET_AUTH,
-    auth: {}
-  }
-}
+  return async function (dispatch) {
+    try {
+      localStorage.removeItem(TOKEN);
+      history.push("/login");
+      dispatch(SetAuth({}));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
 
-
-
-export default function(state = initialState, action){
-  switch(action.type){
+export default function (state = initialState, action) {
+  switch (action.type) {
     case SET_AUTH:
-      return action.auth
+      return action.auth;
     default:
-      return state
+      return state;
   }
 }
