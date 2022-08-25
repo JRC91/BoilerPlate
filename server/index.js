bcf3d9df1f5e450c7f90b8699e9eae561a166213
JRC91 <jimcarmody91@gmail.com>
2022-08-25 13:02:49 -0400

    added gatekeep

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, '../public/')))
 app.use(morgan('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/auth', require('./router/router'))
+app.use('/auth', require('./router/auth'))
 
 
 const init = async () => {
